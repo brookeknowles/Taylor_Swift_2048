7551064bbd1f02268ae4d758aad75c2da216f0e6
Brooke Knowles <brookeknowles7@gmail.com>
2022-04-18 21:32:44 +1200

    added some swift math. still need to add the swift logic to the move functions for it to work properly

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ document.addEventListener('DOMContentLoaded', () =>  {
     let squares = []
     const width = 4
     let score = 0
+
+    // album consts
+    const album_debut = "<img src=\"/images/debut.png\" width=\"100px\" height=\"100px\">"
+    const album_fearless = "<img src=\"/images/fearless.png\" width=\"100px\" height=\"100px\">"
+    const album_speaknow = "<img src=\"/images/speaknow.png\" width=\"100px\" height=\"100px\">"
+    const album_red = "<img src=\"/images/red.png\" width=\"100px\" height=\"100px\">"
+    const album_1989 = "<img src=\"/images/1989.png\" width=\"100px\" height=\"100px\">"
+    const album_reputation = "<img src=\"/images/reputation.png\" width=\"100px\" height=\"100px\">"
+    const album_lover = "<img src=\"/images/lover.png\" width=\"100px\" height=\"100px\">"
+    const album_folklore = "<img src=\"/images/folklore.png\" width=\"100px\" height=\"100px\">"
+    const album_evermore = "<img src=\"/images/evermore.png\" width=\"100px\" height=\"100px\">"
+    const album_fearlessTV = "<img src=\"/images/fearless_TV.png\" width=\"100px\" height=\"100px\">"
+    const album_redTV = "<img src=\"/images/red_TV.png\" width=\"100px\" height=\"100px\">"
   
     // create the game board
     function createBoard() {
@@ -23,7 +36,8 @@ document.addEventListener('DOMContentLoaded', () =>  {
     function generate() {
       randomNumber = Math.floor(Math.random() * squares.length)
       if (squares[randomNumber].innerHTML == 0) {
-        squares[randomNumber].innerHTML = 2
+        // squares[randomNumber].innerHTML = 2
+        squares[randomNumber].innerHTML ="<img src=\"/images/debut.png\" width=\"100px\" height=\"100px\">";
         checkForGameOver()
       } else generate()
     }
@@ -37,7 +51,8 @@ document.addEventListener('DOMContentLoaded', () =>  {
           let totalThree = squares[i+2].innerHTML
           let totalFour = squares[i+3].innerHTML
           let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
-  
+            console.log(row)
+
           let filteredRow = row.filter(num => num)
           let missing = 4 - filteredRow.length
           let zeros = Array(missing).fill(0)
@@ -115,16 +130,92 @@ document.addEventListener('DOMContentLoaded', () =>  {
         squares[i+(width*3)].innerHTML = newColumn[3]
       }
     }
+
+    // function calculates which album comes after 2 of the same are combined
+    // inputs will be in form squares[i].innerHTML
+    function swiftieMath(tileA, tileB) {
+        if (tileA == tileB == album_debut){
+            // this will be the combinedTotal
+            return album_fearless
+        }
+        else if (tileA == tileB == album_fearless){
+            return album_speaknow
+        }
+        else if (tileA == tileB == album_speaknow){
+            return album_red
+        }
+        else if (tileA == tileB == album_red){
+            return album_1989
+        }
+        else if (tileA == tileB == album_1989){
+            return album_reputation
+        }
+        else if (tileA == tileB == album_reputation){
+            return album_lover
+        }
+        else if (tileA == tileB == album_lover){
+            return album_folklore
+        }
+        else if (tileA == tileB == album_folklore){
+            return album_evermore
+        }
+        else if (tileA == tileB == album_evermore){
+            return album_fearlessTV
+        }
+        else if (tileA == tileB == album_fearlessTV){
+            return album_redTV
+        }
+    }
+
+    // calculates score since images of albums dont add as well as integers
+    function scoreCalculation(combinedTotal){
+        if (combinedTotal == album_debut){
+            return 2
+        }
+        if (combinedTotal == album_fearless){
+            return 4
+        }
+        if (combinedTotal == album_speaknow){
+            return 8
+        }
+        if (combinedTotal == album_red){
+            return 16
+        }
+        if (combinedTotal == album_1989){
+            return 32
+        }
+        if (combinedTotal == album_reputation){
+            return 64
+        }
+        if (combinedTotal == album_lover){
+            return 128
+        }
+        if (combinedTotal == album_folklore){
+            return 256
+        }
+        if (combinedTotal == album_evermore){
+            return 512
+        }
+        if (combinedTotal == album_fearlessTV){
+            return 1024
+        }
+        if (combinedTotal == album_redTV){
+            return 2048
+        }
+    }
   
     // combine two tiles that are next to eachother (left/right)
     // i.e. when 2 8s meet they join to make 1 16
     function combineRow() {
       for (let i =0; i < 15; i++) {
         if (squares[i].innerHTML === squares[i +1].innerHTML) {
-          let combinedTotal = parseInt(squares[i].innerHTML) + parseInt(squares[i +1].innerHTML)
+          // let combinedTotal = parseInt(squares[i].innerHTML) + parseInt(squares[i +1].innerHTML) 
+          // adding ints works but not adding images. will need to add some swiftiemath
+          // let combinedTotal = swiftieMath(squares[i].innerHTML, squares[i+1].innerHTML)
+          let combinedTotal = swiftieMath(squares[i].innerHTML, squares[i +1].innerHTML)
           squares[i].innerHTML = combinedTotal
           squares[i +1].innerHTML = 0
-          score += combinedTotal
+          score += scoreCalculation(combinedTotal)      // need some swiftiemath for this one too
           scoreDisplay.innerHTML = score
         }
       }
@@ -136,10 +227,11 @@ document.addEventListener('DOMContentLoaded', () =>  {
     function combineColumn() {
       for (let i =0; i < 12; i++) {
         if (squares[i].innerHTML === squares[i +width].innerHTML) {
-          let combinedTotal = parseInt(squares[i].innerHTML) + parseInt(squares[i +width].innerHTML)
+          // let combinedTotal = parseInt(squares[i].innerHTML) + parseInt(squares[i +width].innerHTML)
+          let combinedTotal = swiftieMath(squares[i].innerHTML, squares[i +width].innerHTML)
           squares[i].innerHTML = combinedTotal
           squares[i +width].innerHTML = 0
-          score += combinedTotal
+          score += scoreCalculation(combinedTotal)
           scoreDisplay.innerHTML = score
         }
       }
@@ -189,9 +281,11 @@ document.addEventListener('DOMContentLoaded', () =>  {
     }
   
     // check for the number 2048 in the squares to win
+    // check for red TV in the squares to win
     function checkForWin() {
       for (let i=0; i < squares.length; i++) {
-        if (squares[i].innerHTML == 2048) {
+        // if (squares[i].innerHTML == 2048) {
+        if (squares[i].innerHTML == "<img src=\"/images/red_TV.png\" width=\"100px\" height=\"100px\">"){
           resultDisplay.innerHTML = 'You WIN'
           document.removeEventListener('keyup', control)
           setTimeout(() => clear(), 3000)
@@ -219,26 +313,44 @@ document.addEventListener('DOMContentLoaded', () =>  {
       clearInterval(myTimer)
     }
   
+    // add taylor swift
+    function addTaylorSwift() {
+        for (let i=0; i < squares.length; i++) {
+          if (squares[i].innerHTML == 0) squares[i].style.backgroundColor = '#afa192'
+          else if (squares[i].innerHTML == 2) squares[i].innerHTML =album_debut
+          else if (squares[i].innerHTML  == 4) squares[i].innerHTML =album_fearless
+          else if (squares[i].innerHTML  == 8) squares[i].innerHTML =album_speaknow
+          else if (squares[i].innerHTML  == 16) squares[i].innerHTML =album_red
+          else if (squares[i].innerHTML  == 32) squares[i].innerHTML =album_1989
+          else if (squares[i].innerHTML == 64) squares[i].innerHTML =album_reputation
+          else if (squares[i].innerHTML == 128) squares[i].innerHTML =album_lover
+          else if (squares[i].innerHTML == 256) squares[i].innerHTML =album_folklore
+          else if (squares[i].innerHTML == 512) squares[i].innerHTML =album_evermore
+          else if (squares[i].innerHTML == 1024) squares[i].innerHTML =album_fearlessTV
+          else if (squares[i].innerHTML == 2048) squares[i].innerHTML =album_redTV
+        }
+    }
+    addTaylorSwift()
   
     // add colours
-    function addColours() {
-      for (let i=0; i < squares.length; i++) {
-        if (squares[i].innerHTML == 0) squares[i].style.backgroundColor = '#afa192'
-        else if (squares[i].innerHTML == 2) squares[i].style.backgroundColor = '#eee4da'
-        else if (squares[i].innerHTML  == 4) squares[i].style.backgroundColor = '#ede0c8' 
-        else if (squares[i].innerHTML  == 8) squares[i].style.backgroundColor = '#f2b179' 
-        else if (squares[i].innerHTML  == 16) squares[i].style.backgroundColor = '#ffcea4' 
-        else if (squares[i].innerHTML  == 32) squares[i].style.backgroundColor = '#e8c064' 
-        else if (squares[i].innerHTML == 64) squares[i].style.backgroundColor = '#ffab6e' 
-        else if (squares[i].innerHTML == 128) squares[i].style.backgroundColor = '#fd9982' 
-        else if (squares[i].innerHTML == 256) squares[i].style.backgroundColor = '#ead79c' 
-        else if (squares[i].innerHTML == 512) squares[i].style.backgroundColor = '#76daff' 
-        else if (squares[i].innerHTML == 1024) squares[i].style.backgroundColor = '#beeaa5' 
-        else if (squares[i].innerHTML == 2048) squares[i].style.backgroundColor = '#d7d4f0' 
-      }
-  }
-  addColours()
+//     function addColours() {
+//       for (let i=0; i < squares.length; i++) {
+//         if (squares[i].innerHTML == 0) squares[i].style.backgroundColor = '#afa192'
+//         else if (squares[i].innerHTML == 2) squares[i].style.backgroundColor = '#eee4da'
+//         else if (squares[i].innerHTML  == 4) squares[i].style.backgroundColor = '#ede0c8' 
+//         else if (squares[i].innerHTML  == 8) squares[i].style.backgroundColor = '#f2b179' 
+//         else if (squares[i].innerHTML  == 16) squares[i].style.backgroundColor = '#ffcea4' 
+//         else if (squares[i].innerHTML  == 32) squares[i].style.backgroundColor = '#e8c064' 
+//         else if (squares[i].innerHTML == 64) squares[i].style.backgroundColor = '#ffab6e' 
+//         else if (squares[i].innerHTML == 128) squares[i].style.backgroundColor = '#fd9982' 
+//         else if (squares[i].innerHTML == 256) squares[i].style.backgroundColor = '#ead79c' 
+//         else if (squares[i].innerHTML == 512) squares[i].style.backgroundColor = '#76daff' 
+//         else if (squares[i].innerHTML == 1024) squares[i].style.backgroundColor = '#beeaa5' 
+//         else if (squares[i].innerHTML == 2048) squares[i].style.backgroundColor = '#d7d4f0' 
+//       }
+//   }
+//   addColours()
   
-  var myTimer = setInterval(addColours, 50)
+  var myTimer = setInterval(addTaylorSwift, 50)
   
   })
